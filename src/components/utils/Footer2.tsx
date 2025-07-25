2f63bdedc8b5a59255f733e10cc3f3b43207f1bf
UlrichDevp <ulrichsoh72@gmail.com>
2025-07-25 18:15:22 +0100

    fix:hover

--- src/components/utils/Footer2.tsx
+++ src/components/utils/Footer2.tsx
@@ -1,11 +1,11 @@
 export default function Footer2(){
     return(
         <div className="flex justify-between items-center text-bgbutton text-[1vw] font-bold">
-            <div className="flex gap-1 items-center">
+            <a className="flex gap-1 items-center">
                 <img src="/arrow.svg" alt="arrow" />
                 <span>vos details</span>
-            </div>
-            <span>Signin</span>
+            </a>
+            <a>Signin</a>
         </div>
     )
 }
--- src/components/utils/Timeline.tsx
+++ src/components/utils/Timeline.tsx
@@ -12,19 +12,19 @@ type timelineProps = {
 const Timeline: React.FC<timelineProps> = ({ image, titre, description, link }) => {
 
     return (
-        <div className="flex items-center gap-2 h-[6vh]">
+        <div className="flex items-center gap-2 h-[6vh] ">
 
-            <div className="flex items-center  justify-center border-[1px] border-title1  shadow-sm rounded-[6px] w-[4vw] h-full">
-                <Link to={link} className="w-full h-full text-center flex items-center  justify-center">
+            <div className="flex items-center  justify-center border-[1px] border-title1  shadow-sm rounded-[6px] w-[4vw] h-full opacity-50 hover:opacity-100">
+                <Link to={link} className="w-full h-full text-center flex items-center  justify-center opacity-50 hover:opacity-100">
                     <img className="w-[2vw] h-[3vh]" src={`/${image}.svg`} alt="user" />
                 </Link>
             </div>
 
             <div className="flex flex-col font-semibold text-title1  text-[1vw]">
-                <h1 className=""><Link to={link} className="w-full h-full text-center">
+                <h1 className=""><Link to={link} className="w-full h-full text-center opacity-50 hover:opacity-100">
                     {titre}
                 </Link></h1>
-                <h3 className="">{description}</h3>
+                <h3 className="opacity-50">{description}</h3>
             </div>
         </div>
     )
